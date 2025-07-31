022147f953ebb6a15b8e05eedf0d8e49a3a6679a
BeaverlyGithub <beaverlyapp@gmail.com>
2025-07-31 20:45:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+
+const CACHE_NAME = 'chilla-app';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/css/base.css',
+  '/css/auth.css',
+  '/css/dashboard.css',
+  '/js/auth.js',
+  '/js/dashboard.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
