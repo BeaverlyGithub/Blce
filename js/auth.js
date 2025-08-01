76870535fc9da5e1b73d3fb4bb0d4d0058fd1bc2
BeaverlyGithub <beaverlyapp@gmail.com>
2025-08-01 04:30:37 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -224,8 +224,8 @@ class ChillaAuth {
 
     async handleGoogleAuth() {
         const clientId = "514107671303-canjqpiuhlk97eigl1o9cv24i1bjpe54.apps.googleusercontent.com";
-        const redirectUri = `${API_BASE}/auth/callback`;
-        const scope = "https://www.googleapis.com/auth/userinfo.email";
+        const redirectUri = "https://cook.beaverlyai.com/auth/callback";
+        const scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid";
         const oauthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=online&prompt=select_account`;
 
         window.location.href = oauthUrl;
